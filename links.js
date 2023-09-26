cf58e35658948e4028cfc00a1d6c43c0249078f8
Anuolu-2020 <Anuolu1645@gmail.com>
2023-09-26 19:00:01 +0100

    Reworked links to images

---
+++ links.js
@@ -0,0 +1,20 @@
+export const songLinks = {
+  reason:
+    "https://archive.org/download/mbid-1da8bbef-1950-4172-a565-7c84ebe8c0dc/mbid-1da8bbef-1950-4172-a565-7c84ebe8c0dc-35986079889_thumb250.jpg",
+  workOfArt:
+    "https://ia902709.us.archive.org/28/items/mbid-1da8bbef-1950-4172-a565-7c84ebe8c0dc/mbid-1da8bbef-1950-4172-a565-7c84ebe8c0dc-35986079889_thumb250.jpg",
+  timeless:
+    "https://archive.org/download/mbid-cb508fc4-728c-43f1-abc0-5af0deb87596/mbid-cb508fc4-728c-43f1-abc0-5af0deb87596-35372207120_thumb250.jpg",
+  charm: "./charm.jpeg",
+  holiday: "./holiday.jpeg",
+  Ngozi: "./Ngozi.jpeg",
+  modupe: "./modupe.jpeg",
+  manOfTheYear: "./manOfTheYear",
+  chemical: "./chemical.jpeg",
+};
+
+export const otherLinks = {
+  profile_pic: "https://reqres.in/img/faces/7-image.jpg",
+};
+
+export let name = "Emmanuel";
--- script2.js
+++ script2.js
@@ -1,3 +1,19 @@
+import { songLinks, name, otherLinks } from "./links.js";
+
+console.log(songLinks.reason, name);
+
+let profile_pic = document.getElementById("profile-img");
+profile_pic.src = otherLinks.profile_pic;
+
+let settingsImg = document.getElementById("settings-profile");
+settingsImg.src = otherLinks.profile_pic;
+
+let songCards = document.querySelectorAll(".card");
+
+songCards.forEach((card) => {
+  card.style.backgroundImage = `url(${songLinks.workOfArt})`;
+});
+
 function sidebarCollapse() {
   let sidebarTexts = document.querySelectorAll(".sidebar-text");
   sidebarTexts.forEach(function (sidebarText) {
@@ -17,3 +33,6 @@ function profileToggle() {
     profilebar.style.display = "none";
   }
 }
+
+document.getElementById("profile-img").addEventListener("click", profileToggle);
+document.getElementById("main-menu").addEventListener("click", sidebarCollapse);
