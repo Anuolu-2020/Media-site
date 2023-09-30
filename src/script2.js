f281423f73bd0edbf3f6619da6e04181b8a56b7b
Anuolu-2020 <Anuolu1645@gmail.com>
2023-09-30 15:39:17 +0100

    Addded mobile device detection for the play, favorite and add to library buttons

--- src/script2.js
+++ src/script2.js
@@ -205,47 +205,58 @@ libraryIcon.forEach((library) => {
   library.src = "./ICONS/library.png";
 });
 
+//For Detecting mobile Screen
+function hasAccelerometer() {
+  return "DeviceMotionEvent" in window;
+}
+function isTouchDevice() {
+  return "ontouchstart" in window || navigator.maxTouchPoints;
+}
+
 songCards.forEach((card) => {
-  card.addEventListener("mouseenter", () => {
-    let playIcon = card.querySelector(".play-button");
-    let favoriteIcon = card.querySelector(".favorite-button");
-    let libraryIcon = card.querySelector(".add-button");
+  let playIcon = card.querySelector(".play-button");
+  let favoriteIcon = card.querySelector(".favorite-button");
+  let libraryIcon = card.querySelector(".add-button");
 
-    playIcon.style.opacity = "0.7";
-    favoriteIcon.style.opacity = "0.7";
-    libraryIcon.style.opacity = "0.7";
-  });
+  if (hasAccelerometer() && isTouchDevice()) {
+    playIcon.style.opacity = "1";
+    favoriteIcon.style.opacity = "1";
+    libraryIcon.style.opacity = "1";
+  } else {
+    card.addEventListener("mouseenter", () => {
+      playIcon.style.opacity = "0.7";
+      favoriteIcon.style.opacity = "0.7";
+      libraryIcon.style.opacity = "0.7";
+    });
 
-  card.addEventListener("mouseleave", () => {
-    let playIcon = card.querySelector(".play-button");
-    let favoriteIcon = card.querySelector(".favorite-button");
-    let libraryIcon = card.querySelector(".add-button");
-
-    playIcon.style.opacity = "0";
-    favoriteIcon.style.opacity = "0";
-    libraryIcon.style.opacity = "0";
-  });
+    card.addEventListener("mouseleave", () => {
+      playIcon.style.opacity = "0";
+      favoriteIcon.style.opacity = "0";
+      libraryIcon.style.opacity = "0";
+    });
+  }
 });
 
 albumsCards.forEach((albmCard) => {
-  console.log(albmCard);
-  albmCard.addEventListener("mouseenter", () => {
-    let playIcon = albmCard.querySelector(".play-button");
-    let favoriteIcon = albmCard.querySelector(".favorite-button");
-    let libraryIcon = albmCard.querySelector(".add-button");
+  let playIcon = albmCard.querySelector(".play-button");
+  let favoriteIcon = albmCard.querySelector(".favorite-button");
+  let libraryIcon = albmCard.querySelector(".add-button");
 
-    playIcon.style.opacity = "0.7";
-    favoriteIcon.style.opacity = "0.7";
-    libraryIcon.style.opacity = "0.7";
-  });
+  if (hasAccelerometer() && isTouchDevice()) {
+    playIcon.style.opacity = "1";
+    favoriteIcon.style.opacity = "1";
+    libraryIcon.style.opacity = "1";
+  } else {
+    albmCard.addEventListener("mouseenter", () => {
+      playIcon.style.opacity = "0.7";
+      favoriteIcon.style.opacity = "0.7";
+      libraryIcon.style.opacity = "0.7";
+    });
 
-  albmCard.addEventListener("mouseleave", () => {
-    let playIcon = albmCard.querySelector(".play-button");
-    let favoriteIcon = albmCard.querySelector(".favorite-button");
-    let libraryIcon = albmCard.querySelector(".add-button");
-
-    playIcon.style.opacity = "0";
-    favoriteIcon.style.opacity = "0";
-    libraryIcon.style.opacity = "0";
-  });
+    albmCard.addEventListener("mouseleave", () => {
+      playIcon.style.opacity = "0";
+      favoriteIcon.style.opacity = "0";
+      libraryIcon.style.opacity = "0";
+    });
+  }
 });
