8d322094e84671c01f0c8d411c8b803a358e87f4
Anuolu-2020 <Anuolu1645@gmail.com>
2023-10-02 18:32:02 +0100

    Added interactivity between audioIcon and volume

--- src/audioPlayer.js
+++ src/audioPlayer.js
@@ -4,11 +4,12 @@ let playPauseBtn = document.getElementById("playPauseBtn");
 let nextBtn = document.getElementById("nextBtn");
 let artistName = document.getElementById("artistName");
 let songName = document.getElementById("songName");
-let volumeBtn = document.getElementById("volumeBtn");
+let volume = document.getElementById("volumeBtn");
 let audioIcon = document.getElementById("volumeIcon");
 let repeatBtn = document.getElementById("repeatBtn");
 let shuffleBtn = document.getElementById("shuffleBtn");
 let audioCollaspe = document.getElementById("audioCollapse");
+let audioPlayer = document.getElementById("audioPlayer");
 
 //link the icons to the button
 previousBtn.src = "./ICONS/previous.png";
@@ -18,3 +19,23 @@ audioIcon.src = "./ICONS/volume-up.png";
 repeatBtn.src = "./ICONS/replay.png";
 shuffleBtn.src = "./ICONS/shuffle.png";
 audioCollaspe.src = "./ICONS/expand-audio.png";
+
+//For Detecting mobile Screen
+function hasAccelerometer() {
+  return "DeviceMotionEvent" in window;
+}
+function isTouchDevice() {
+  return "ontouchstart" in window || navigator.maxTouchPoints;
+}
+
+if (hasAccelerometer() && isTouchDevice()) {
+  volume.style.visibility = "visible";
+} else {
+  audioIcon.addEventListener("mouseenter", () => {
+    volume.style.visibility = "visible";
+  });
+
+  audioPlayer.addEventListener("mouseleave", () => {
+    volume.style.visibility = "hidden";
+  });
+}
