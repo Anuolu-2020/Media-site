ed830cec09cdade11bfc0dc66ff83a7042aa16ae
Anuolu-2020 <Anuolu1645@gmail.com>
2023-10-02 15:18:29 +0100

    Updated Audiobar with appropriate icons

---
+++ src/audioPlayer.js
@@ -0,0 +1,20 @@
+// Get all the references to the audio player button
+let previousBtn = document.getElementById("previousBtn");
+let playPauseBtn = document.getElementById("playPauseBtn");
+let nextBtn = document.getElementById("nextBtn");
+let artistName = document.getElementById("artistName");
+let songName = document.getElementById("songName");
+let volumeBtn = document.getElementById("volumeBtn");
+let audioIcon = document.getElementById("volumeIcon");
+let repeatBtn = document.getElementById("repeatBtn");
+let shuffleBtn = document.getElementById("shuffleBtn");
+let audioCollaspe = document.getElementById("audioCollapse");
+
+//link the icons to the button
+previousBtn.src = "./ICONS/previous.png";
+playPauseBtn.src = "./ICONS/play.png";
+nextBtn.src = "./ICONS/next.png";
+audioIcon.src = "./ICONS/volume-up.png";
+repeatBtn.src = "./ICONS/replay.png";
+shuffleBtn.src = "./ICONS/shuffle.png";
+audioCollaspe.src = "./ICONS/expand-audio.png";
