ca88d0b174574b303c161252548bb5d440edd3b8
Anuolu-2020 <Anuolu1645@gmail.com>
2023-10-04 19:15:17 +0100

    audio player functionalities completed

--- src/audioPlayer.js
+++ src/audioPlayer.js
@@ -1,4 +1,5 @@
 import { musicLinks } from "./musicLinks.js";
+import { songLinks, songArtists, topSongTitle } from "./links.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
 import {
   getStorage,
@@ -14,8 +15,8 @@ import { firebaseConfig } from "./fireStoreConnect.js";
 let previousBtn = document.getElementById("previousBtn");
 let playPauseBtn = document.getElementById("playPauseBtn");
 let nextBtn = document.getElementById("nextBtn");
-let artistName = document.getElementById("artistName");
-let songName = document.getElementById("songName");
+let artistName = document.getElementById("audioArtist");
+let songName = document.getElementById("audioName");
 let volume = document.getElementById("volumeBtn");
 let audioIcon = document.getElementById("volumeIcon");
 let repeatBtn = document.getElementById("repeatBtn");
@@ -32,7 +33,6 @@ audioIcon.src = "./ICONS/volume-up.png";
 repeatBtn.src = "./ICONS/replay.png";
 shuffleBtn.src = "./ICONS/shuffle.png";
 audioCollaspe.src = "./ICONS/expand-audio.png";
-songImg.src = "../songs-cover/work of art.jpeg"; //place holder source
 
 //For Detecting mobile Screen
 function hasAccelerometer() {
@@ -66,6 +66,29 @@ let topSongsUrl = [
   musicLinks.reason,
 ];
 
+let songCover = [
+  songLinks.workOfArt,
+  songLinks.Ngozi,
+  songLinks.modupe,
+  songLinks.charm,
+  songLinks.reason,
+];
+
+let topSongName = [
+  topSongTitle.num1,
+  topSongTitle.num2,
+  topSongTitle.num3,
+  topSongTitle.num4,
+  topSongTitle.num5,
+];
+
+let topSongArtist = [
+  songArtists.art2,
+  songArtists.art4,
+  songArtists.art3,
+  songArtists.art1,
+  songArtists.art7,
+];
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
@@ -74,7 +97,10 @@ const storage = getStorage(app);
 
 let audio = document.getElementById("audio");
 let currentSongIndex = 0;
-//"/Songs/Ngozi - Crayon&Ayra Starr.m4a"
+let isRepeatActive = false; //Track repeat state
+let isPlaying = false; //Track Playing state
+let isShuffleActive = false; //Track Shuffle state
+
 //topSongs fetch
 function playSong(index) {
   const fileRef = ref(storage, topSongsUrl[index]);
@@ -82,24 +108,80 @@ function playSong(index) {
     let url = URL.createObjectURL(blob);
     audio.src = url;
     audio.play();
+    isPlaying = true;
+    playPauseBtn.src = "./ICONS/pause.png";
   });
+}
+
+playPauseBtn.addEventListener("click", () => {
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    audio.play();
+    isPlaying = true;
+    playPauseBtn.src = "./ICONS/pause.png";
+  }
+});
+
+function pauseSong() {
+  audio.pause();
+  isPlaying = false;
+  playPauseBtn.src = "./ICONS/play.png";
 }
 
 // Add event listeners to play buttons
 playButton.forEach((btn, idx) => {
   btn.addEventListener("click", () => {
+    songImg.src = songCover[idx];
+    songName.innerHTML = topSongName[idx];
+    artistName.innerHTML = topSongArtist[idx];
     currentSongIndex = idx; // Update the current song index
     playSong(currentSongIndex);
   });
 });
 
-// Listen for the 'ended' event on the audio element to play the next song
-audio.addEventListener("ended", () => {
-  // Increment the current song index and play the next song
-  currentSongIndex = (currentSongIndex + 1) % topSongsUrl.length;
-  playSong(currentSongIndex);
+//Repeat button
+repeatBtn.addEventListener("click", () => {
+  isRepeatActive = !isRepeatActive;
+  repeatBtn.src = isRepeatActive
+    ? "./ICONS/replay-checked.png"
+    : "./ICONS/replay.png";
 });
 
+//Shuffle button
+shuffleBtn.addEventListener("click", () => {
+  isShuffleActive = !isShuffleActive;
+  shuffleBtn.src = isShuffleActive
+    ? "./ICONS/shuffle-checked.png"
+    : "./ICONS/shuffle.png";
+
+  if (isShuffleActive) {
+    for (let i = topSongsUrl.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [topSongsUrl[i], topSongsUrl[j]] = [topSongsUrl[j], topSongsUrl[i]];
+    }
+  }
+});
+
+// Listen for the 'ended' event on the audio element to play the next song
+audio.addEventListener("ended", () => {
+  if (isRepeatActive) {
+    audio.currentTime = 0;
+    audio.play();
+  } else if (isShuffleActive) {
+    currentSongIndex = Math.floor(Math.random() * topSongsUrl.length);
+    playSong(currentSongIndex);
+  } else {
+    // Increment the current song index and play the next song
+    currentSongIndex = (currentSongIndex + 1) % topSongsUrl.length;
+    songName.innerHTML = topSongName[currentSongIndex];
+    songImg.src = songCover[currentSongIndex];
+    artistName.innerHTML = topSongArtist[currentSongIndex];
+    playSong(currentSongIndex);
+  }
+});
+
+//previous button to go to the previoua song
 previousBtn.addEventListener("click", () => {
   // decrement the current song index and play the next song
   if (currentSongIndex === 0) {
@@ -107,12 +189,62 @@ previousBtn.addEventListener("click", () => {
   } else {
     currentSongIndex = (currentSongIndex - 1) % topSongsUrl.length;
   }
+  songName.innerHTML = topSongName[currentSongIndex];
+  songImg.src = songCover[currentSongIndex];
+  artistName.innerHTML = topSongArtist[currentSongIndex];
+
   playSong(currentSongIndex);
 });
 
+//next button to go to the next song
 nextBtn.addEventListener("click", () => {
   // Increment the current song index and play the next song
   currentSongIndex =
     (currentSongIndex - 1 + topSongsUrl.length) % topSongsUrl.length;
+  songName.innerHTML = topSongName[currentSongIndex];
+  songImg.src = songCover[currentSongIndex];
+  artistName.innerHTML = topSongArtist[currentSongIndex];
+
   playSong(currentSongIndex);
+});
+
+//audio volume
+volume.addEventListener("input", () => {
+  audio.volume = volume.value;
+});
+
+//audio duration information
+let songDuration = document.getElementById("songDuration");
+//formatting the time
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+  return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+}
+
+//audio playback progress
+let progressSlide = document.getElementById("progressSlide");
+let isSeeking = false; // Track if user is dragging the progress slider
+progressSlide.addEventListener("input", () => {
+  // Update the audio's currentTime based on the progress slider's value
+  const newTime = (progressSlide.value / 100) * audio.duration;
+  audio.currentTime = newTime;
+});
+
+progressSlide.addEventListener("change", () => {
+  // Update the audio's currentTime based on the final progress slider value
+  const newTime = (progressSlide.value / 100) * audio.duration;
+  audio.currentTime = newTime;
+});
+
+//audio time update
+audio.addEventListener("timeupdate", () => {
+  // Update the progress slider and current time display
+  if (!isSeeking) {
+    const progress = (audio.currentTime / audio.duration) * 100;
+    progressSlide.value = progress;
+    songDuration.innerText = `${formatTime(audio.currentTime)} / ${formatTime(
+      audio.duration
+    )}`;
+  }
 });
