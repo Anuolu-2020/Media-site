820023795a629c997b39a38d5e769b969ab7ed7f
Anuolu-2020 <Anuolu1645@gmail.com>
2023-10-23 18:58:02 +0100

    Implemented switching between lyrics and up next song in playlist

--- src/audioScreen.js
+++ src/audioScreen.js
@@ -27,6 +27,38 @@ let progressSlide = document.getElementById("progressSlide");
 let rotationDegree = 180;
 let isPlaylistVisible = false;
 
+let upNextSelect = document.getElementById("upNext-select");
+let lyricsSelect = document.getElementById("lyrics-select");
+
+let playlist = document.getElementById("playlist");
+let lyrics = document.getElementById("lyrics");
+
+//Is upnext currently selected
+let isPlaylistUpNext = true;
+
+if (isPlaylistUpNext) {
+  upNextSelect.style.borderBottom = "2px solid white";
+}
+
+//when u click on lyrics
+lyricsSelect.addEventListener("click", () => {
+  isPlaylistUpNext = false;
+  lyricsSelect.style.borderBottom = "2px solid white";
+  upNextSelect.style.borderBottom = "";
+  playlist.style.display = "none";
+  lyrics.style.display = "flex";
+  console.log("click");
+});
+
+//When u click on up next
+upNextSelect.addEventListener("click", () => {
+  isPlaylistUpNext = true;
+  upNextSelect.style.borderBottom = "2px solid white";
+  playlist.style.display = "flex";
+  lyrics.style.display = "none";
+  lyricsSelect.style.borderBottom = "";
+});
+
 //For constructing of songs ui in the playlist sidebar
 export function playlistTopSongUi() {
   if (isPlaylistVisible) {
