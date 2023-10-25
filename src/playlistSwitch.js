1d665d0f36668522d296d0ae3596da452fb07146
Anuolu-2020 <Anuolu1645@gmail.com>
2023-10-25 19:00:46 +0100

    Implemented top song lyrics handler and started work for gospel song lyric handler

--- src/playlistSwitch.js
+++ src/playlistSwitch.js
@@ -1,3 +1,5 @@
+// import { lyricHandler } from "./audioPlayer.js";
+
 let upNextSelect = document.getElementById("upNext-select");
 let lyricsSelect = document.getElementById("lyrics-select");
 
@@ -28,3 +30,54 @@ upNextSelect.addEventListener("click", () => {
   lyrics.style.display = "none";
   lyricsSelect.style.borderBottom = "";
 });
+
+//Fetch handler for top song lyrics
+let topSongsLyricsData = async () => {
+  try {
+    const response = await fetch("./Lyrics/topSongs.json");
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error reading JSON file:", error);
+  }
+};
+
+//Fetch handler for gospel songs
+let gospelSongsLyricsData = async () => {
+  try {
+    const response = await fetch("./Lyrics/gospelSongs.json");
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error reading JSON file:", error);
+  }
+};
+
+(async () => {
+  const lyric = await gospelSongsLyricsData();
+  console.log(lyric["goodnessOfGod"]);
+})();
+
+/////////////////////////////////////////////////
+////LYRICS HANDLER//////////////////
+
+export function topSongLyricHandler(index) {
+  let topSongLyric = ["Basquiat", "Ngozi", "Modupe", "Charm", "Reason"];
+
+  // Call the async function to read the JSON file
+  (async () => {
+    const lyric = await topSongsLyricsData();
+
+    lyrics.innerText = JSON.stringify(lyric[`${topSongLyric[index]}`], 2);
+  })();
+}
