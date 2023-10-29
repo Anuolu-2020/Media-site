bcf5c2a7f3e5cf6e9b81c1a999093f2f5fe50738
Ogunleye Emmanuel <Anuolu1645@gmail.com>
2023-10-29 22:07:55 +0100

    library button early stages started

---
+++ src/libraryBtn.js
@@ -0,0 +1,15 @@
+// let isLibraryClicked = false;
+
+// let libraryButtons = document.querySelectorAll(".add-button");
+
+// libraryButtons.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     if (isLibraryClicked) {
+//       isLibraryClicked = !isLibraryClicked;
+//       btn.src = "./ICONS/library.png";
+//     } else {
+//       isLibraryClicked = !isLibraryClicked;
+//       btn.src = "./ICONS/library-checked.png";
+//     }
+//   });
+// });
