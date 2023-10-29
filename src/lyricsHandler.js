167ff8a8d8868a85ad8c4e1fbc8dbe6c1245396f
Ogunleye Emmanuel <Anuolu1645@gmail.com>
2023-10-29 22:08:42 +0100

    Lyrics handler for albums and artist songs implemented

--- src/lyricsHandler.js
+++ src/lyricsHandler.js
@@ -32,6 +32,38 @@ let gospelSongsLyricsData = async () => {
   }
 };
 
+//Fetch handler for album songs
+let albumSongsLyricsData = async () => {
+  try {
+    const response = await fetch("./Lyrics/albumSongs.json");
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error reading JSON file:", error);
+  }
+};
+
+//Fetch handler for album songs
+let artistSongsLyricsData = async () => {
+  try {
+    const response = await fetch("./Lyrics/artistSongs.json");
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error reading JSON file:", error);
+  }
+};
+
 /////////////////////////////////////////////////
 ////LYRICS HANDLER//////////////////
 
@@ -58,5 +90,65 @@ export function gospelSongLyricHandler(index) {
   (async () => {
     const lyric = await gospelSongsLyricsData();
     lyrics.innerText = lyric[`${gospelSongLyric[index]}`];
+  })();
+}
+
+export function albumSongLyricHandler(index, songIndex) {
+  let albumLyricKey = [
+    "trenchToTriumph",
+    "thyKingdomCome",
+    "timeless",
+    "boyAlone",
+    "raveRoses",
+  ];
+
+  (async () => {
+    const albumLyricData = await albumSongsLyricsData();
+    let songLyric = albumLyricData[`${albumLyricKey[index]}`];
+
+    let trenchToTriumph = ["Ngozi", "Modupe", "belleFull", "ijoLaba"];
+    let thyKingdomCome = ["manOfYear", "dejavu", "Karma"];
+    let timeless = ["unavailable", "feel", "noCompettion", "inTheGarden"];
+    let boyAlone = ["reason", "comeCloser", "itYours", "soso"];
+    let raveRoses = ["calmDown", "Charm", "Holiday"];
+
+    let albumSongs = [
+      trenchToTriumph,
+      thyKingdomCome,
+      timeless,
+      boyAlone,
+      raveRoses,
+    ];
+
+    lyrics.innerText = songLyric[`${albumSongs[index][songIndex]}`];
+  })();
+}
+
+export function artistSongLyricHandler(index, songIndex) {
+  let artistLyricKey = [
+    "Rema",
+    "Asake",
+    "Crayon",
+    "SeyiVibez",
+    "Davido",
+    "omahLay",
+    "postMalone",
+  ];
+
+  (async () => {
+    const songLyricData = await artistSongsLyricsData();
+    let songLyric = songLyricData[`${artistLyricKey[index]}`];
+
+    let Crayon = ["Ngozi", "Modupe", "belleFull", "ijoLaba"];
+    let SeyiVibez = ["manOfYear", "dejavu", "Karma"];
+    let davido = ["unavailable", "feel", "noCompettion", "inTheGarden"];
+    let omahLay = ["reason", "comeCloser", "itYours", "soso"];
+    let rema = ["calmDown", "Charm", "Holiday"];
+    let asake = ["Basquiat", "lonelyAtTheTop", "2:30", "Remember", "Sunshine"];
+    let postMalone = ["Chemical", "betterNow", "Circles", "Sunflower"];
+
+    let artists = [rema, asake, Crayon, SeyiVibez, davido, omahLay, postMalone];
+
+    lyrics.innerText = songLyric[`${artists[index][songIndex]}`];
   })();
 }
